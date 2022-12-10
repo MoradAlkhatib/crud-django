425e6a4628b1d35ba33678e3a8b07984eaae2acd
moradalkhatib <muradalkhateb@icloud.com>
2022-12-10 17:52:32 +0300

    create book model and add it to the admin

--- books/models.py
+++ books/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 
-# Create your models here.
+
+
+class Book(models.Model):
+    
+    name = models.CharField(max_length=200)
+    page = models.IntegerField()
+    purchaser = models.ForeignKey(get_user_model(),on_delete=models.CASCADE)
+    description = models.TextField(default='The Description Here...')
+    image = models.CharField(max_length=255,default="No Image Provider")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('book_edit', kwargs={'pk': self.pk})
